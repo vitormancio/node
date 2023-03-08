22dbb76f0015e4ff4d86dd963dad51b699cbfdcf
vitordev <vitor.mancio@gmail.com>
2023-03-08 18:31:15 -0300

    develop a simple API server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 import http from 'http'
-
 const server = http.createServer(
     (req,res) => {
-        res.end('Fucking do it, lets goo')
+    const users = []
+    const { url, method } = req
+    if (method === 'GET' && url === '/users') {
+        res
+        .setHeader("Content-Type","application/json")
+        .end(JSON.stringify(users))
+    }  
+    if(method === 'POST' && url === '/users') {
+        res.end('put you users')
+        users.push({
+            id:1,
+            name:'vitor',
+            email:'vitor@web3dev.com'
+        })
+    }         
     }
 )
-
 server.listen(3030)
